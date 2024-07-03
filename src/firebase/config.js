419130b722a8808ca9c42dcc671087e2e9d65217
dr250385 <dr250385@WIN-10M20X2>
2024-07-03 14:35:13 +0200

    Creating a firebase project and connecting to firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAX9f5EZmtfBKbI_17V9rUmISs_Nz0GhKQ',
+  authDomain: 'muso-86ef8.firebaseapp.com',
+  projectId: 'muso-86ef8',
+  storageBucket: 'muso-86ef8.appspot.com',
+  messagingSenderId: '845014415488',
+  appId: '1:845014415488:web:92359df6f3e6a830aa4e65'
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+// timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectFirestore, projectAuth, timestamp };
