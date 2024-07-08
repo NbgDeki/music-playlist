d052356026019b1987dd56a5b819f9810be18d90
dr250385 <dr250385@WIN-10M20X2>
2024-07-08 16:15:20 +0200

    Added logout functionality

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
 <template>
+  <navbar></navbar>
   <div class="content">
     <router-view />
   </div>
 </template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    Navbar
+  }
+};
+</script>
 
 <style lang="scss">
 .content {
---
+++ src/components/Navbar.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="navbar">
+    <nav>
+      <img src="@/assets/ninja.png" alt="Ninja" />
+      <h1><router-link :to="{ name: 'home' }">Muso Ninjas</router-link></h1>
+      <div class="links">
+        <button @click="handleClick">Logout</button>
+        <router-link class="btn" :to="{ name: 'signup' }">Sign up</router-link>
+        <router-link class="btn" :to="{ name: 'login' }">Log in</router-link>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+import useLogout from '@/composables/useLogout';
+import { useRouter } from 'vue-router';
+
+export default {
+  name: 'Navbar',
+
+  setup() {
+    const { logout } = useLogout();
+    const router = useRouter();
+
+    const handleClick = async () => {
+      await logout();
+      router.push({ name: 'login' });
+    };
+
+    return {
+      handleClick
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  padding: 16px 10px;
+  margin-bottom: 60px;
+  background: #fff;
+}
+
+nav {
+  display: flex;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;
+
+  h1 {
+    margin-left: 20px;
+  }
+
+  .links {
+    margin-left: auto;
+
+    a,
+    button {
+      margin-left: 16px;
+      font-size: 14px;
+    }
+  }
+  img {
+    max-height: 60px;
+  }
+}
+</style>
