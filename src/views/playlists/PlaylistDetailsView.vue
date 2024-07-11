3d5d6c0f0daee78a0f6fa8acb95d2215d27bf798
dr250385 <dr250385@WIN-10M20X2>
2024-07-11 17:16:43 +0200

    Added deleting playlist composable

--- src/views/playlists/PlaylistDetailsView.vue
+++ src/views/playlists/PlaylistDetailsView.vue
@@ -9,6 +9,7 @@
       <h2>{{ playlist.title }}</h2>
       <p class="username">Created by {{ playlist.userName }}</p>
       <p class="description">{{ playlist.description }}</p>
+      <button v-if="ownership" @click="handleDelete">Delete Playlist</button>
     </div>
 
     <!-- song list -->
@@ -20,6 +21,9 @@
 
 <script>
 import getDocument from '@/composables/getDocument';
+import getUser from '@/composables/getUser';
+import useDocument from '@/composables/useDocument';
+import { computed } from 'vue';
 
 export default {
   name: 'PlaylistDetails',
@@ -29,8 +33,20 @@ export default {
   setup(props) {
     const { id } = props;
     const { error, document: playlist } = getDocument('playlists', id);
+    const { user } = getUser();
+    const { deleteDoc } = useDocument('playlists', id);
 
-    return { error, playlist };
+    const ownership = computed(() => {
+      return (
+        playlist.value && user.value && user.value.uid === playlist.value.userId
+      );
+    });
+
+    const handleDelete = async () => {
+      await deleteDoc();
+    };
+
+    return { error, playlist, ownership, handleDelete };
   }
 };
 </script>
